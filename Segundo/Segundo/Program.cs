3aa3918b628c99d28fb420660a4d7f980371888f
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-11 08:53:31 -0300

    Create Primeiro Segundo and Terceiro

---
+++ Segundo/Segundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Segundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Área de um trapézio
+            double baseMenor = 6.0, baseMaior = 8.0, altura = 5.0;
+            double area = (baseMenor + baseMaior) * altura / 2.0;
+
+            Console.WriteLine(area);
+
+            //Área de um trapézio
+            float baseMenorF = 6.0f, baseMaiorF = 8.0f, alturaF = 5.0f;
+            float areaF = (baseMenorF + baseMaiorF) * alturaF / 2.0f;
+
+            Console.WriteLine(areaF);
+        }
+    }
+}
---
+++ Terceiro/Terceiro/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Terceiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double resultado;
+
+            a = 5;
+            b = 2;
+
+            resultado = a / b;
+
+            //Trunca o resultado para inteiro
+            Console.WriteLine(resultado); //2
+
+            resultado = (double) a / b;
+
+            //Para que não aconteça precisamos fazer um casting
+            Console.WriteLine(resultado); //2.5
+
+            double x;
+            int y;
+
+            x = 5.0;
+            // y = x; //Assim não dá, precisamos fazer um casting
+            y = (int)x;
+
+            Console.WriteLine(y); //
+        }
+    }
+}
