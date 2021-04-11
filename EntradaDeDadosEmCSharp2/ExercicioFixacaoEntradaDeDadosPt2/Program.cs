e0210676541e0895c5fac576a09e1e6a8ecf2c2d
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-11 08:53:01 -0300

    Create EntradaDeDadosEmCSharp 1 and 2

---
+++ EntradaDeDadosEmCSharp2/ExercicioFixacaoEntradaDeDadosPt2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFixacaoEntradaDeDadosPt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Quantos quartos tem na sua casa?");
+            int quarto = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com seu último nome, idade e altura (mesma linha): ");
+            string[] vet = Console.ReadLine().Split(" ");
+            string n = vet[0];
+            int age = int.Parse(vet[1]);
+            double h = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(nome);
+            Console.WriteLine(quarto);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(n);
+            Console.WriteLine(age);
+            Console.WriteLine(h.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ EntradasDeDadosEmCSharp/EntradasDeDadosEmCSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EntradasDeDadosEmCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////string frase = "Bom dia!", x = "Vermelho", y = "Green", z = "Blue", a = "Amarelo", b = "Laranja", c = "Cinza";
+            //Console.Write("Digite uma frase: ");
+            //string frase = Console.ReadLine();
+
+            //Console.Write("Digite 6 cor: ");
+            //string x = Console.ReadLine();
+            //string y = Console.ReadLine();
+            //string z = Console.ReadLine();
+            //string a = Console.ReadLine();
+            //string b = Console.ReadLine();
+            //string c = Console.ReadLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("Você digitou: ");
+            //Console.WriteLine(frase);
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            //SPLIT
+            //Podemos simplificar ainda mais, removendo a variável s e passando o Console.ReadLine().Split(""); diretamente para o vet.
+            Console.WriteLine("Digite três palavras em sequência: ");
+            //string s = Console.ReadLine();
+
+            string[] vet = Console.ReadLine().Split(" ");
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }
+    }
+}
