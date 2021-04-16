8761e9fa38f327463f122f4703dd1550cdb06090
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-15 21:00:43 -0300

    Update Startup.cs

--- ASPNETCore20/Aulas/Aula1/CasaDoCodigo/Startup.cs
+++ ASPNETCore20/Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -22,7 +22,6 @@ namespace CasaDoCodigo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Você acabou de desenvolver o modelo de dados e também o contexto do Entity Framework Core através da classe ApplicationContext, que implementa a classe-base DbContext, para realizar as operações para obter/gravar dados no seu banco de dados Sql Server através das entidades da aplicação.
             string connectionString = Configuration.GetConnectionString("Default");
 
             services.AddMvc();
@@ -53,12 +52,7 @@ namespace CasaDoCodigo
                     template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
 
-            //cria o banco de dados e seus objetos (tabelas, campos, índices, etc) usando apenas as entidades e mapeamentos da aplicação.
-            //serviceProvider.GetService<ApplicationContext>().Database.EnsureCreated(); //Garante que o banco de dados foi criado ao iniciar a aplicação. Uma vez que o banco de dados foi gerado através do método EnsureCreated(), a aplicação não pode mais usar migrations.Por isso, a não ser que uma aplicação seja pequena e destinada a testes, é recomendável usar sempre o método Migrate().
-
-            //método Migrate() realiza exatamente a mesma tarefa, porém usando apenas as migrações da aplicação.
-            //serviceProvider.GetService<ApplicationContext>().Database.Migrate(); //Garante que o banco de dados foi criado ao iniciar a aplicação
-            serviceProvider.GetService<DataService>().InicializaDB(); //Garante que o banco de dados foi criado ao iniciar a aplicação
+            serviceProvider.GetService<DataService>().InicializaDB();
         }
     }
 
