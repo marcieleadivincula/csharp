22f9986bf6108a787566f264cc6d4e8aa9aa2a66
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:21:42 -0300

    Create Bytebank6

---
+++ CSharpEOrientacaoAObjetos/6-Bytebank/ContaCorrente.cs
@@ -0,0 +1,67 @@
+﻿using _06_ByteBank;
+
+namespace _6_Bytebank
+{
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+        private double _saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+            }
+        }
+
+        //Criando primeira função/método
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+
+                //contaDestino._saldo += valor;
+                contaDestino.Depositar(valor);
+
+                return true;
+            }
+        }
+    }
+}
---
+++ CSharpEOrientacaoAObjetos/6-Bytebank/Program.cs
@@ -0,0 +1,27 @@
+﻿using _06_ByteBank;
+using System;
+
+namespace _6_Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+            cliente.Nome = "Guilherme";
+            cliente.CPF = "434.564.879-20";
+            cliente.Profissao = "Desenvolvedor";
+
+            //conta.SetSaldo(-10);
+
+            //Console.WriteLine(conta.GetSaldo());
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+}
