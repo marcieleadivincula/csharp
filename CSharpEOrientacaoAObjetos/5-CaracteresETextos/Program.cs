0fc1c0618d9bf03218e155ab6506ebb79e964a14
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:23:48 -0300

    Create CaracteresETextos

---
+++ CSharpEOrientacaoAObjetos/5-CaracteresETextos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres E Textos");
+            Console.WriteLine("========================================================");
+
+            //character
+            char primeiraLetra = 'a';
+            Console.WriteLine("Caracter = " + primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine("65 = " + primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine("Primeiraletra + 1 = " + primeiraLetra);
+
+            string texto = "Olá mundo!";
+            Console.WriteLine("Texto = " + texto);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2020;
+            string cursosProgramacao = @"- .NET 
+-  Java
+- JavaScript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            string saudacao = "Olá, meu nome é ";
+            string nome = "Rômulo ";
+            string continuacao = "e minha idade é ";
+            int idade = 45;
+            Console.WriteLine(saudacao + nome + continuacao + idade);
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
