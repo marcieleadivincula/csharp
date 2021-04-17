2e1e089ed411d6f463fb3e5bc78694424ef76f00
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:20:56 -0300

    Create Bytebank2

---
+++ CSharpEOrientacaoAObjetos/2-Bytebank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _2_Bytebank
+{
+    public class ContaCorrente
+    {
+        public string _titular;
+        public int _agencia;
+        public int _numero;
+        public double _saldo;
+    }
+}
---
+++ CSharpEOrientacaoAObjetos/2-Bytebank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta._titular = "Gabriela";
+
+            Console.WriteLine(conta._titular); // default ""
+            Console.WriteLine(conta._agencia); // default 0
+            Console.WriteLine(conta._numero); // default 0
+            Console.WriteLine(conta._saldo); // default 0
+        }
+    }
+}
