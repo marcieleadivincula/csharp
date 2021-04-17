e6151bcac45407493db468b371a8f10659e3df6d
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:29:12 -0300

    Create Funcionario.cs

---
+++ CSharpEOrientacaoAObjetos/CSharpParte3EntendendoHerancaEInterface/Funcionarios/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class Funcionario
+    {
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+        
+        public Funcionario(double salario, string cpf)
+        {
+            Console.WriteLine("Criando FUNCIONARIO");
+
+            CPF = cpf;
+            Salario = salario;
+
+            TotalDeFuncionarios++;
+        }
+        
+        public abstract void AumentarSalario();
+
+        public abstract double GetBonificacao();
+    }
+}
