15b2d366121b4137bd87b1dc8134aed1412c71ca
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:29:17 -0300

    Create Diretor.cs

---
+++ CSharpEOrientacaoAObjetos/CSharpParte3EntendendoHerancaEInterface/Funcionarios/Diretor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+    {
+        public Diretor(string cpf) : base(5000, cpf)
+        {
+            Console.WriteLine("Criando DIRETOR");
+        }
+        
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.5;
+        }
+    }
+}
