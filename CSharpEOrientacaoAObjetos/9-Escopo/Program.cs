fbf96675b99c68aeda6b2adf9f95a57541a246f2
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:24:35 -0300

    Create Escopo

---
+++ CSharpEOrientacaoAObjetos/9-Escopo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+            Console.WriteLine("========================================================");
+
+            int idade = 16;
+            bool acompanhado = false;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "Não está acompanhado!";
+            }
+
+
+            if (idade >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
