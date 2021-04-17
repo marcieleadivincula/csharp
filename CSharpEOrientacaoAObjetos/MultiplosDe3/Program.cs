8bcec252cb5ff8bedce76b9ad033c064b9da9699
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:25:46 -0300

    Create MultiplosDe3

---
+++ CSharpEOrientacaoAObjetos/MultiplosDe3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MultiplosDe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto - Desafio Opcional: Múltiplos de 3");
+            //Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+
+            for (int numero = 1; numero < 100; numero++)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+            Console.WriteLine();
+
+            //Outra forma
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
