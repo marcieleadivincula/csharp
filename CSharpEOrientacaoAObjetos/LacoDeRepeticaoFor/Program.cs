a3e567dabcec4b5809c9ed9064f7b4cdb1b3ee87
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:26:09 -0300

    Create LacosDeRepeticaoFor

---
+++ CSharpEOrientacaoAObjetos/LacoDeRepeticaoFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LacoDeRepeticaoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto - Mão na massa: Aprofundando laços");
+            Console.WriteLine("========================================================");
+
+            //Escreva um for encadeado que imprima a tabuada de cada número, nosso código ficará assim:
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                for (int contador = 0; contador <= 10; contador++)
+                {
+                    Console.Write(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
---
+++ CSharpEOrientacaoAObjetos/LacoDeRepeticaoFor2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LacoDeRepeticaoFor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto - Mão na massa: Aprofundando laços");
+            Console.WriteLine("========================================================");
+
+            //Vamos praticar o comando break imprimindo uma matriz triangular, esse é o nosso código:
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    if (coluna > linha)
+                    {
+                        break;
+                    }
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            //Podemos melhorar nosso código, para isso faça as modificações:
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna <= linha; coluna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
