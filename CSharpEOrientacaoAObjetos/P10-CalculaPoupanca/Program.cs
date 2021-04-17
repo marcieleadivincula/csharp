8f07e7559762fe5f79b0b9bb381f339253cfc8ab
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:25:30 -0300

    Create CalculaPoupanca

---
+++ CSharpEOrientacaoAObjetos/P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P10_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupança");
+            Console.WriteLine("========================================================");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses,  você terá " + valorInvestido);
+                //contadorMes = contadorMes + 1;
+                //contadorMes += 1;
+                contadorMes++;
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
---
+++ CSharpEOrientacaoAObjetos/P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11 - Calcula Poupança 2");
+            Console.WriteLine("========================================================");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses,  você terá " + valorInvestido);
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
