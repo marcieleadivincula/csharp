14b1ebcb11d2fc785f185d3abb573369efc84a22
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:24:06 -0300

    Create AtribuicoesDeVariaveis

---
+++ CSharpEOrientacaoAObjetos/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6 - Atribuições de Variáveis");
+            Console.WriteLine("========================================================");
+
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 35;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeGustavo);
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
