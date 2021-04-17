d45dafb5edc31f9b4800bac888ee2ad2bdf5156f
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:26:36 -0300

    Create ForEncadeado

---
+++ CSharpEOrientacaoAObjetos/P13-ForEncadeado/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - For Encadeado");
+            Console.WriteLine("========================================================");
+            //*
+            //**
+            //***
+
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    Console.Write("*");
+                    //if (coluna == linha)
+                    //{
+                    //    break;
+                    //}
+
+                    if (coluna >= linha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Outro forma
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna <= linha; coluna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            //Com número
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    if (coluna > linha)
+                    {
+                        break;
+                    }
+                    Console.Write(coluna + 1);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
