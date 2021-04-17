08262f361a29f0c5b75c6a67fe9dd856bcb1d0a1
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:24:48 -0300

    Create AliquotaIR

---
+++ CSharpEOrientacaoAObjetos/AliquotaIR/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AliquotaIR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto  Opcional: Alíquota com ifs");
+            Console.WriteLine("========================================================");
+
+            int idade = 16;
+            bool acompanhado = false;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "Está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "Não está acompanhado!";
+            }
+
+
+            if (idade >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
