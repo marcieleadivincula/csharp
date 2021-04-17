8cd7efc2e06e5f9b96032372041e32758047a9bf
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:21:17 -0300

    Create Bytebank4

---
+++ CSharpEOrientacaoAObjetos/4-Bytebank/ContaCorrente.cs
@@ -0,0 +1,46 @@
+﻿namespace _4_Bytebank
+{
+    public class ContaCorrente
+    {
+        public string _titular;
+        public int _agencia;
+        public int _numero;
+        public double _saldo;
+
+        //Criando primeira função/método
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldo -= valor;
+
+                //contaDestino._saldo += valor;
+                contaDestino.Depositar(valor);
+
+                return true;
+            }
+        }
+    }
+}
---
+++ CSharpEOrientacaoAObjetos/4-Bytebank/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _4_Bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno._titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno._saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(contaDoBruno._saldo);
+            Console.WriteLine("Resultado do saque: " + resultadoSaque);
+            Console.WriteLine(contaDoBruno._saldo);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno._saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela._titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno._saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela._saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno._saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela._saldo);
+
+            Console.WriteLine("Resultado da transferência: " + resultadoTransferencia);
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno._saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela._saldo);
+        }
+    }
+}
