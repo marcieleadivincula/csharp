bfd375762d7535f37f79a7d138de1b177734a130
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-16 22:22:27 -0300

    Create CriandoVariaveis

---
+++ CSharpEOrientacaoAObjetos/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+            int idade = 32;
+            Console.WriteLine("A idade é: " + idade);
+
+            idade = 10 + 5;
+            Console.WriteLine("Idade = " + idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine("Idade = " + idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Idade = " + idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+        }
+    }
+}
